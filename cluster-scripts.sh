0f39cf66dbb0d6f2581b3707cdd445fb1366223e
Tushar Sharma <tush4r@users.noreply.github.com>
2019-02-12 11:24:37 +0530

    Update cluster-scripts.sh
    test changes

--- cluster-scripts.sh
+++ cluster-scripts.sh
@@ -5,8 +5,4 @@
 # username=`cat parameters.json | jq -r '.parameters.clusterLoginUserName.value'`
 
 cd ~/
-git clone https://github.cerner.com/ts066692/OJDBC.git
-cd OJDBC/
-HDP_PATH=$(ls -d /usr/hdp/* | head -n 1)
-SQOOP_PATH=$HDP_PATH/sqoop/lib/
-sudo cp ojdbc6.jar $SQOOP_PATH
+echo 'Hello World'
