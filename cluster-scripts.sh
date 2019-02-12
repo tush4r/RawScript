1cf93f5afff4a458c9653cb1a67e5330478a3e4a
Tushar Sharma <tush4r@users.noreply.github.com>
2019-02-12 12:18:42 +0530

    Update cluster-scripts.sh
    Git clone add

--- cluster-scripts.sh
+++ cluster-scripts.sh
@@ -3,7 +3,7 @@
 #This Script shall install required drivers on the clusters
 
 cd ~/
-https://github.com/tush4r/RawScript.git
+git clone https://github.com/tush4r/RawScript.git
 cd RawScript/
 HDP_PATH=$(ls -d /usr/hdp/* | head -n 1)
 SQOOP_PATH=$HDP_PATH/sqoop/lib/
